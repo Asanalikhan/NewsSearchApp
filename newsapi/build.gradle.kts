79c706df50949685e7f917c84a772e62ca11d0ef
Assanalikhan <210103042@stu.sdu.edu.kz>
2024-03-12 17:13:08 +0500

    commit

---
+++ newsapi/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("java-library")
+    id("org.jetbrains.kotlin.jvm")
+}
+dependencies{
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("androidx.annotation:annotation:1.7.1")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
+}
