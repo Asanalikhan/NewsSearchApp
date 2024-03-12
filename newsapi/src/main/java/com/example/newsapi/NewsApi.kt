915bc3b492c07a7412fd5d21962514b3576f38bf
Assanalikhan <210103042@stu.sdu.edu.kz>
2024-03-12 17:28:07 +0500

    models

--- newsapi/src/main/java/com/example/newsapi/NewsApi.kt
+++ newsapi/src/main/java/com/example/newsapi/NewsApi.kt
@@ -1,7 +1,8 @@
 package com.example.newsapi
 
 import androidx.annotation.IntRange
-import kotlinx.serialization.SerialName
+import com.example.newsapi.models.Languages
+import com.example.newsapi.models.SortBy
 import retrofit2.http.GET
 import retrofit2.http.Query
 import java.util.Date
@@ -13,17 +14,11 @@ interface NewsApi {
         @Query("q") query: String? = null,
         @Query("from") from: Date? = null,
         @Query("to") to:Date? = null,
-        @Query("to") languages: String? = null,
+        @Query("to") languages: List<Languages>? = null,
         @Query("sortBy") sortBy: SortBy? = null,
         @Query("pageSize") @IntRange(from = 0, to = 100) pageSize: Int = 100,
         @Query("page") @IntRange(from = 1) page: Int = 1,
         )
 }
-enum class SortBy {
-    @SerialName("relevancy")
-    RELEVANCY,
-    @SerialName("popularity")
-    POPULARITY,
-    @SerialName("publishedAt")
-    PUBLISHED_AT
-}
+
+
