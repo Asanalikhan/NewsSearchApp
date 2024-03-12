73155eb0d6c421b85b9a934eb9ae6c0b006bce81
Assanalikhan <210103042@stu.sdu.edu.kz>
2024-03-12 17:54:27 +0500

    NewsApi

--- newsapi/src/main/java/com/example/newsapi/NewsApi.kt
+++ newsapi/src/main/java/com/example/newsapi/NewsApi.kt
@@ -5,6 +5,9 @@ import com.example.newsapi.models.Article
 import com.example.newsapi.models.Languages
 import com.example.newsapi.models.Responce
 import com.example.newsapi.models.SortBy
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Query
 import java.util.Date
@@ -21,6 +24,18 @@ interface NewsApi {
         @Query("pageSize") @IntRange(from = 0, to = 100) pageSize: Int = 100,
         @Query("page") @IntRange(from = 1) page: Int = 1,
         ): Responce<Article>
+}
+
+fun NewsApi(
+    baseUrl: String,
+    okHttpClient: OkHttpClient? = null
+): NewsApi{
+    val retrofit = retrofit(baseUrl, okHttpClient)
+    return retrofit.create()
+}
+private fun retrofit(baseUrl: String, okHttpClient: OkHttpClient?,): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl(baseUrl).run { if (okHttpClient != null) client(okHttpClient) else this }.build()
 }
 
 
